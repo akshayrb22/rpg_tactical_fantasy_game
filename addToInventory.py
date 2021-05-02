846b2bfd9158f0a698d432329b0aa8676c11de21
akshayrb22 <akshay.bhat9812222@gmail.com>
2021-05-03 00:39:25 +0200

    placeItem uses weighted probability to inject items to inventory

--- addToInventory.py
+++ addToInventory.py
@@ -1,23 +1,27 @@
 import xml.etree.ElementTree as ET
+import numpy as np
 from io import BytesIO
 import os
 
 
 def placeItem(difficulty: float, path: str):
+    potion_list = ['life_potion', 'super_life_potion', 'hyper_life_potion', 'ultra_life_potion']
+    lvl_000_weights = [0.5, 0.3, 0.15, 0.05]
+    lvl_025_weights = [0.2, 0.5, 0.2, 0.1]
+    lvl_050_weights = [0.2, 0.2, 0.4, 0.2]
+    lvl_075_weights = [0.05, 0.15, 0.5, 0.3]
     item = None
-
     if difficulty == 0.0:
-        item = 'key'
+        item = np.random.choice(potion_list, p=lvl_000_weights)
     elif difficulty == 0.25:
-        item = 'door_key'
+        item = np.random.choice(potion_list, p=lvl_025_weights)
     elif difficulty == 0.5:
-        item = 'life_potion'
+        item = np.random.choice(potion_list, p=lvl_050_weights)
     elif difficulty == 0.75:
-        item = 'vigor_potion'
+        item = np.random.choice(potion_list, p=lvl_075_weights)
     else:
         print('Invalid difficulty.')
         exit()
-
     tree = ET.parse(path)
     tree.find('.//inventory/item').text = item
     tree.write(path)
